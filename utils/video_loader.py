612740425ec5386d6dfd7276048de313ec14fde7
Morteza Abolghasemi <a.re.morteza@gmail.com>
2023-04-14 12:12:41 +0330

    first commit

---
+++ utils/video_loader.py
@@ -0,0 +1,10 @@
+import pytube
+
+
+def download_video_from_youtube(video_id, destination_path=None, video_name=None):
+    destination_path = destination_path if destination_path else '/content/'
+    video_name = video_name if video_name else f'{video_id}.mp4'
+    print(f'Downloading YouTube video {video_id}.')
+    pytube.YouTube(f'https://youtu.be/{video_id}').streams.get_highest_resolution().download(
+        destination_path, filename=video_name)
+    print(f'Download complete.')
