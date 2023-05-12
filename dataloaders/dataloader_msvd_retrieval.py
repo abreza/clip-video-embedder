677a6bd9879cbbf2223c3d40d4a562400a9b6d2a
Morteza Abolghasemi <a.re.morteza@gmail.com>
2023-05-12 17:51:16 +0330

    fix: there is no need to segment msvd videos

--- dataloaders/dataloader_msvd_retrieval.py
+++ dataloaders/dataloader_msvd_retrieval.py
@@ -65,5 +65,5 @@ class MSVD_DataLoader(Dataset):
         [start, end] = data['segments'][0]['timestamp']
         descriptions = [item['description'] for item in data['segments']]
         video_path = f"{self.features_path}/{data['youtube_video_id']}_{start}_{end}.avi"
-        images = self.rawVideoExtractor.get_video_data(video_path, start, end)
+        images = self.rawVideoExtractor.get_video_data(video_path)
         return images, descriptions
